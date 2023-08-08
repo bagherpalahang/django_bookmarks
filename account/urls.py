f2a237df790f98414d8a01821a3f54d2a8f73485
bagher <palahangmohammadbagher@gmail.com>
2023-08-09 03:34:10 +0430

    adding change password view

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from django.contrib.auth import views
-from .views import user_login
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path('login/', user_login, name='login'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('login/', views.LogoutView.as_view(), name='login'),
+    # path('login/', user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('', views.dashboard, name='dashboard')
 ]
